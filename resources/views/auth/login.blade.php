fc4267e5f965ff9b2c355427d498897365d9f469
Unknown <Tomas.Wandahl@live.se>
2018-02-06 22:56:01 +0100

    Styled Login-forms

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,17 +5,17 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
+                <div class="panel-heading" style="font-size: 50px;margin-bottom: 15px;">Login</div>
 
                 <div class="panel-body">
                     <form class="form-horizontal" method="POST" action="{{ route('login') }}">
                     {{ csrf_field() }}
 
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+                            <label for="email" class="col-md-4 control-label" style="font-size: 30px;">E-Mail Address</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus style = "width: 400px;padding: 12px 20px;margin: 8px 0;box-sizing: border-box;">
 
                                 @if ($errors->has('email'))
                                     <span class="help-block">
@@ -26,10 +26,10 @@
                         </div>
 
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
+                            <label for="password" class="col-md-4 control-label" style="font-size: 30px;">Password</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
+                                <input id="password" type="password" class="form-control" name="password" required style = "width: 400px;padding: 12px 20px;margin: 8px 0;box-sizing: border-box;">
 
                                 @if ($errors->has('password'))
                                     <span class="help-block">
